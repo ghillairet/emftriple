68df6dd4924cc450aad0b5f5b4b8fffff238aa69
ghillairet <g.hillairet@gmail.com>
2011-04-28 14:54:51 +0100

    support for simple sparql query api added Sparql.java added SparqlString.java

---
+++ bundles/com.emftriple/src/com/emftriple/query/Sparql.java
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Guillaume Hillairet.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Guillaume Hillairet - initial API and implementation
+ *******************************************************************************/
+package com.emftriple.query;
+
+import static com.emftriple.query.Sparql.OptionalGraphPattern.optional;
+import static com.emftriple.query.Sparql.TripleGraphPattern.triple;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+
+public class Sparql implements ETripleQuery {
+	
+	private List<String> select;
+	private GraphPattern[] where;
+	private Map<String, String> prefixes;
+	
+	public Sparql() {
+		select = new ArrayList<String>();
+		prefixes = new HashMap<String, String>();
+	}
+	
+	public Sparql prefix(String prefix, String uri) {
+		prefixes.put(prefix, uri);
+		return this;
+	}
+	
+	public Sparql select(String select) {
+		this.select.add(select);
+		return this;
+	}
+	
+	public Sparql where(GraphPattern... patterns) {
+		if (this.where != null) {
+			throw new IllegalArgumentException();
+		}
+		this.where = patterns;
+		return this;
+	}
+	
+	public String get() {
+		String res = "";
+		for (String p: prefixes.keySet()) {
+			res+="prefix "+p+": <"+prefixes.get(p)+"> ";
+		}
+		res+= "select ";
+		for (String s: select)
+			res+=s+" ";
+		res+="where { ";
+		for (GraphPattern g: where)
+			res+=g.get();
+		res+="}";
+		return res;
+	}
+		
+	public interface GraphPattern {
+		String get();
+	}
+	
+	public static class TripleGraphPattern implements GraphPattern {
+		private String s;
+		private String p;
+		private String o;
+
+		TripleGraphPattern(String s, String p, String o) {
+			this.s = s;
+			this.p = p;
+			this.o = o;
+		}
+		
+		public static GraphPattern triple(String s, String p, String o) {
+			return new TripleGraphPattern(s, p, o);
+		}
+		
+		public String get() {
+			return s+" "+p+" "+o+" . ";
+		}
+	}
+	
+	public static class OptionalGraphPattern implements GraphPattern {
+		private GraphPattern[] patterns;
+
+		OptionalGraphPattern(GraphPattern... patterns) {
+			this.patterns = patterns;
+		}
+		
+		public static GraphPattern optional(GraphPattern... patterns) {
+			return new OptionalGraphPattern(patterns);
+		}
+		
+		public String get() {
+			String res = "optional { ";
+				for (GraphPattern p: patterns)
+					res+=p.get();
+			res+=" } ";
+			return res;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(
+				new Sparql()
+				.select("?s")
+				.select("?p")
+				.select("?o")
+				.where( 
+						triple("?s", "?p", "?o"),
+						optional(
+								triple("?s", "?p", "?o"),
+								triple("?o", "?p", "?s") 
+						) )
+				.toURI(URI.createURI("emftriple://data?graph=http://graph")));
+	}
+
+	@Override
+	public URI toURI(URI resourceURI) {
+		String query = get().replaceAll(" ", "%20").replaceAll("#", "%23");
+		return URI.createURI(resourceURI+"&query="+query);
+	}
+
+}
