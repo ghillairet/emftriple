996220c52975cd3a8372c923497b41131e7ddb64
g.hillairet <g.hillairet@gmail.com>
2011-05-31 21:38:28 +0100

    start update restlet extension

--- bundles/com.emftriple.restlet/src/com/emftriple/restlet/ETripleRestletUtil.java
+++ bundles/com.emftriple.restlet/src/com/emftriple/restlet/ETripleRestletUtil.java
@@ -10,18 +10,6 @@
  *******************************************************************************/
 package com.emftriple.restlet;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.restlet.data.MediaType;
-import org.restlet.ext.rdf.RdfRepresentation;
-
-import com.emf4sw.rdf.Triple;
-import com.emftriple.resource.ETripleResourceImpl;
-import com.emftriple.transform.IPutObject;
-import com.emftriple.transform.impl.PutObjectImpl;
 
 /**
  * 
@@ -30,33 +18,34 @@ import com.emftriple.transform.impl.PutObjectImpl;
  */
 public class ETripleRestletUtil {
 	
-	public static RdfRepresentation getRepresentation(Resource resource, MediaType mediaType) {
-		final IPutObject put = new PutObjectImpl((ETripleResourceImpl) resource);
-		
-		final Collection<Triple> triples = new ArrayList<Triple>();
-		for (EObject obj: resource.getContents())
-			triples.addAll(put.put(obj));
-		
-		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(triples), mediaType);
-	}
+//	public static RdfRepresentation getRepresentation(Resource resource, MediaType mediaType) {
+//		final IPutObject put = new PutObjectImpl((ETripleResourceImpl) resource);
+//		
+//		final Collection<Triple> triples = new ArrayList<Triple>();
+//		for (EObject obj: resource.getContents())
+//			triples.addAll(put.put(obj));
+//		
+//		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(triples), mediaType);
+//	}
+//	
+//	public static RdfRepresentation getRepresentation(Resource resource, Iterable<? extends EObject> objects, MediaType mediaType) {
+//		final IPutObject put = new PutObjectImpl((ETripleResourceImpl) resource);
+//		
+//		final Collection<Triple> triples = new ArrayList<Triple>();
+//		for (EObject o: objects) {
+//			triples.addAll(put.put(o));
+//		}
+//		
+//		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(triples), mediaType);
+//	}
+//	
+//	public static RdfRepresentation getRepresentation(Resource resource, EObject object, MediaType mediaType) {
+//		final IPutObject put = new PutObjectImpl((ETripleResourceImpl) resource);
+//		
+//		final Collection<Triple> triples = new ArrayList<Triple>();
+//		triples.addAll(put.put(object));
+//		
+//		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(triples), mediaType);
+//	}
 	
-	public static RdfRepresentation getRepresentation(Resource resource, Iterable<? extends EObject> objects, MediaType mediaType) {
-		final IPutObject put = new PutObjectImpl((ETripleResourceImpl) resource);
-		
-		final Collection<Triple> triples = new ArrayList<Triple>();
-		for (EObject o: objects) {
-			triples.addAll(put.put(o));
-		}
-		
-		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(triples), mediaType);
-	}
-	
-	public static RdfRepresentation getRepresentation(Resource resource, EObject object, MediaType mediaType) {
-		final IPutObject put = new PutObjectImpl((ETripleResourceImpl) resource);
-		
-		final Collection<Triple> triples = new ArrayList<Triple>();
-		triples.addAll(put.put(object));
-		
-		return new RdfRepresentation(RDFGraph2RestletGraph.toGraph(triples), mediaType);
-	}
 }
--- bundles/com.emftriple.restlet/src/com/emftriple/restlet/RDFGraph2RestletGraph.java
+++ bundles/com.emftriple.restlet/src/com/emftriple/restlet/RDFGraph2RestletGraph.java
@@ -12,44 +12,95 @@ package com.emftriple.restlet;
 
 import java.util.Collection;
 
-import org.restlet.data.Language;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.restlet.data.Reference;
 import org.restlet.ext.rdf.Graph;
+import org.restlet.ext.rdf.Literal;
 
-import com.emf4sw.rdf.Literal;
-import com.emf4sw.rdf.Triple;
-import com.emf4sw.rdf.URIElement;
+import com.emftriple.resource.ETripleResource;
+import com.emftriple.transform.Metamodel;
+import com.emftriple.transform.RDFTransform;
 
 /**
  * 
  * @author ghillairet
  * @since 0.1.0
  */
-final class RDFGraph2RestletGraph {
+public class RDFGraph2RestletGraph extends RDFTransform<Reference, Literal, Object, Graph>{
 
 	private RDFGraph2RestletGraph() {}
-	
-	public static Graph toGraph(Collection<Triple> triples) {
-		final Graph rGraph = new Graph();
-		
-		for (Triple t: triples) {
-			Reference sbj = new Reference(((URIElement) t.getSubject()).getURI());
-			Reference prop = new Reference(t.getPredicate().getURI());
-			if (t.getObject() instanceof Literal)
-				rGraph.add(sbj, prop, literal((Literal) t.getObject()));
-			else rGraph.add(sbj, prop, new Reference(((URIElement) t.getObject()).getURI()));
-		}
-		
-		return rGraph;
+
+	@Override
+	public Collection<Object> createTriples(EObject object, String key, Graph graph) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	private static org.restlet.ext.rdf.Literal literal(Literal object) {
-		org.restlet.ext.rdf.Literal lit = new org.restlet.ext.rdf.Literal(object.getLexicalForm());
-		if (object.getDatatype() != null)
-			lit.setDatatypeRef(new Reference(object.getDatatype().getURI()));
-		if (object.getLang() != null)
-			lit.setLanguage(Language.valueOf(object.getLang()));
+	@Override
+	public Reference createURI(String uri, Graph graph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object createTripleURI(Reference subject, Reference predicate, Reference object, Graph graph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object createTripleLiteral(Reference subject, Reference predicate,
+			Literal object, Graph graph) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reference createURI(EObject object, @SuppressWarnings("rawtypes") ETripleResource resource, Graph graph) {
+//		graph.
+		return null;
+	}
+
+	@Override
+	public Reference createURI(EStructuralFeature feature, @SuppressWarnings("rawtypes") ETripleResource resource, Graph graph) {
+		Reference ref = new Reference(Metamodel.INSTANCE.getRdfType(feature));
 		
+		return ref;
+	}
+
+	@Override
+	public Literal createLiteral(EObject object, EAttribute attribute, Object value, Graph graph) {
+		org.restlet.ext.rdf.Literal lit = new org.restlet.ext.rdf.Literal(value.toString());
+//		if (object.getDatatype() != null)
+			lit.setDatatypeRef(new Reference());
+//		if (object.getLang() != null)
+//			lit.setLanguage(Language.valueOf(object.getLang()));
 		return lit;
 	}
+	
+//	public static Graph toGraph(Collection<Triple> triples) {
+//		final Graph rGraph = new Graph();
+//		
+//		for (Triple t: triples) {
+//			Reference sbj = new Reference(((URIElement) t.getSubject()).getURI());
+//			Reference prop = new Reference(t.getPredicate().getURI());
+//			if (t.getObject() instanceof Literal)
+//				rGraph.add(sbj, prop, literal((Literal) t.getObject()));
+//			else rGraph.add(sbj, prop, new Reference(((URIElement) t.getObject()).getURI()));
+//		}
+//		
+//		return rGraph;
+//	}
+//
+//	private static org.restlet.ext.rdf.Literal literal(Literal object) {
+//		org.restlet.ext.rdf.Literal lit = new org.restlet.ext.rdf.Literal(object.getLexicalForm());
+//		if (object.getDatatype() != null)
+//			lit.setDatatypeRef(new Reference(object.getDatatype().getURI()));
+//		if (object.getLang() != null)
+//			lit.setLanguage(Language.valueOf(object.getLang()));
+//		
+//		return lit;
+//	}
 }
