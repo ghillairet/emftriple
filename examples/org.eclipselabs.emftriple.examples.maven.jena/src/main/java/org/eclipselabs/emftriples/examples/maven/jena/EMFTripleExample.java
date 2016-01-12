ae8cdc35bac8ab9dae9a299881ad29dce62215d3
Gabor Szarnyas <szarnyasg@gmail.com>
2016-01-12 14:00:22 +0100

    Jena example added

--- examples/org.eclipselabs.emftriple.examples.maven.jena/src/main/java/org/eclipselabs/emftriples/examples/maven/jena/EMFTripleExample.java
+++ examples/org.eclipselabs.emftriple.examples.maven.jena/src/main/java/org/eclipselabs/emftriples/examples/maven/jena/EMFTripleExample.java
@@ -1,4 +1,4 @@
-package org.eclipselabs.emftriples.examples.maven;
+package org.eclipselabs.emftriples.examples.maven.jena;
 
 import java.io.IOException;
 
@@ -6,17 +6,16 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipselabs.emftriple.jena.resource.RDFResourceFactory;
 import org.eclipselabs.emftriple.junit.model.Book;
 import org.eclipselabs.emftriple.junit.model.ModelFactory;
-import org.eclipselabs.emftriple.sesame.resource.RDFResourceFactory;
 
 public class EMFTripleExample {
 	public static void main(final String[] args) throws IOException {
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("*", new RDFResourceFactory());
 
 		final ResourceSet resourceSet = new ResourceSetImpl();
-		// final Resource r = resourceSet.createResource(URI.createURI("file:///tmp/my.rdf"));
-		final Resource r = resourceSet.createResource(URI.createURI("my.rdf"));
+		final Resource r = resourceSet.createResource(URI.createURI("file:///tmp/my.rdf"));
 
 		final Book b = ModelFactory.eINSTANCE.createBook();
 		b.setTitle("The Book");
